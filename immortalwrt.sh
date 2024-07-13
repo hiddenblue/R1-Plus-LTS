9607e6288ff688e6a2c28ed3c72e1fc62cdbc4ba
hiddenblue <freewayrong@foxmail.com>
2024-07-13 11:21:29 +0800

    增加对lucky插件的支持

--- immortalwrt.sh
+++ immortalwrt.sh
@@ -4,3 +4,5 @@
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
 svn co https://github.com/mingxiaoyu/luci-app-cloudflarespeedtest/trunk/applications/luci-app-cloudflarespeedtest package/luci-app-cloudflarespeedtest
+
+git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
